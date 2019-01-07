dea659d41af99a26e1d448d8c504188505a70d4c
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-01-07 05:55:11 +0100

    Add more binaries

--- internal/provider/github.go
+++ internal/provider/github.go
@@ -1,3 +1,4 @@
+// nolint: lll
 package provider
 
 import "fmt"
@@ -14,7 +15,7 @@ func NewGithubProvider() *Provider {
 					Template: BinaryTemplate{
 						Description: "The simple and easy way to embed static files into Go binaries.",
 						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/packr_{{ .Version }}_{{ .Os }}_{{ .Arch }}.tar.gz"),
-						File:        "packr_{{ .Version }}_{{ .Os }}_{{ .Arch }}/packr",
+						File:        "packr",
 					},
 				},
 			},
@@ -38,14 +39,36 @@ func NewGithubProvider() *Provider {
 					},
 				},
 			},
+			"golangci/golangci-lint": {
+				{
+					VersionConstraint: "*",
+					Template: BinaryTemplate{
+						Homepage:    "http://golangci.com",
+						Description: "Linters Runner for Go",
+						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/golangci-lint-{{ .Version }}-{{ .Os }}-{{ .Arch }}.tar.gz"),
+						File:        "golangci-lint-{{ .Version }}-{{ .Os }}-{{ .Arch }}/golangci-lint",
+					},
+				},
+			},
 			"google/protobuf": {
 				{
 					VersionConstraint: "*",
 					Template: BinaryTemplate{
 						Name:        "protoc",
 						Description: "Protocol Buffers - Google's data interchange format",
-						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/protoc-{{ .Version }}-{{ .Os|protobuf_goos }}-{{ .Arch|protobuf_goarch }}.zip"), // nolint: lll
+						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/protoc-{{ .Version }}-{{ .Os|protobuf_goos }}-{{ .Arch|protobuf_goarch }}.zip"),
 						File:        "bin/protoc",
+					},
+				},
+			},
+			"goph/licensei": {
+				{
+					VersionConstraint: "*",
+					Template: BinaryTemplate{
+						Homepage:    "https://github.com/goph/licensei",
+						Description: "Library and various tools for working with project licenses",
+						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/licensei_{{ .Os }}_{{ .Arch }}.tar.gz"),
+						File:        "licensei",
 					},
 				},
 			},
@@ -56,11 +79,22 @@ func NewGithubProvider() *Provider {
 						Homepage:    "https://goreleaser.github.io/",
 						Description: "Deliver Go binaries as fast and easily as possible",
 						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/goreleaser_{{ .Os | title }}_{{ .Arch | goarch }}.tar.gz"),
-						File:        "goreleaser_{{ .Os | title }}_{{ .Arch | goarch }}/goreleaser",
+						File:        "goreleaser",
 					},
 				},
 			},
-			"mattes/migrate": {
+			"gotestyourself/gotestsum": {
+				{
+					VersionConstraint: "*",
+					Template: BinaryTemplate{
+						Homepage:    "https://github.com/gotestyourself/gotestsum",
+						Description: "The go test runner you never knew you always wanted. Human readable test output, JUnit XML for CI integration, summary of the test run results",
+						URL:         fmt.Sprintf(urlPrefix, "v{{ .Version }}/gotestsum_{{ .Version }}_{{ .Os }}_{{ .Arch }}.tar.gz"),
+						File:        "gotestsum",
+					},
+				},
+			},
+			"golang-migrate/migrate": {
 				{
 					VersionConstraint: "*",
 					Template: BinaryTemplate{
