336ea0f51d4886bb759a279c6c06a971f06f14b4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-01-07 18:23:06 +0100

    Add new binary request format and multibinary install support

--- internal/cli/command/install.go
+++ internal/cli/command/install.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/hashicorp/go-getter" // nolint: goimports
 	"github.com/sagikazarmark/binbrew/internal/provider"
@@ -11,8 +12,7 @@ import (
 )
 
 type installOptions struct {
-	name    string
-	version string
+	binaries []string
 }
 
 // NewInstallCommand returns a cobra command for `binbrew install`.
@@ -20,12 +20,11 @@ func NewInstallCommand() *cobra.Command {
 	options := installOptions{}
 
 	cmd := &cobra.Command{
-		Use:   "install NAME VERSION",
-		Short: "Install a binary",
-		Args:  cobra.ExactArgs(2),
+		Use:   "install NAME[@VERSION]...",
+		Short: "Install one or more binaries",
+		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			options.name = args[0]
-			options.version = args[1]
+			options.binaries = args
 
 			return runInstall(options)
 		},
@@ -37,31 +36,42 @@ func NewInstallCommand() *cobra.Command {
 func runInstall(options installOptions) error {
 	p := provider.NewGithubProvider()
 
-	binary, err := p.Resolve(options.name, options.version)
-	if err != nil {
-		return err
-	}
+	for _, binaryReference := range options.binaries {
+		binaryReferenceParts := strings.SplitN(binaryReference, "@", 2)
 
-	tmp := filepath.Join("bin/tmp", binary.Name)
+		name := binaryReferenceParts[0]
+		version := "latest"
 
-	err = os.MkdirAll(tmp, 0744)
-	if err != nil {
-		return err
-	}
+		if len(binaryReferenceParts) > 1 {
+			version = binaryReferenceParts[1]
+		}
 
-	err = getter.GetAny(tmp, binary.URL)
-	if err != nil {
-		return err
-	}
+		binary, err := p.Resolve(name, version)
+		if err != nil {
+			return err
+		}
 
-	input, err := ioutil.ReadFile(filepath.Join(tmp, binary.File))
-	if err != nil {
-		return err
-	}
+		tmp := filepath.Join("bin/tmp", binary.Name)
 
-	err = ioutil.WriteFile(filepath.Join("bin", binary.Name), input, 0644)
-	if err != nil {
-		return err
+		err = os.MkdirAll(tmp, 0744)
+		if err != nil {
+			return err
+		}
+
+		err = getter.GetAny(tmp, binary.URL)
+		if err != nil {
+			return err
+		}
+
+		input, err := ioutil.ReadFile(filepath.Join(tmp, binary.File))
+		if err != nil {
+			return err
+		}
+
+		err = ioutil.WriteFile(filepath.Join("bin", binary.Name), input, 0644)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
--- tests/vanity_test.sh
+++ tests/vanity_test.sh
@@ -5,19 +5,19 @@ cd "$(dirname "$0")"
 rm -rf bin/
 
 echo "Installing dep 0.5.0"
-../build/binbrew install dep 0.5.0
+../build/binbrew install dep@0.5.0
 
 echo "Installing golangci-lint 1.12.5"
-../build/binbrew install golangci-lint 1.12.5
+../build/binbrew install golangci-lint@1.12.5
 
 echo "Installing protobuf 3.6.1"
-../build/binbrew install protobuf 3.6.1
+../build/binbrew install protobuf@3.6.1
 
 echo "Installing protoc 3.6.1"
-../build/binbrew install protoc 3.6.1
+../build/binbrew install protoc@3.6.1
 
 echo "Installing goreleaser 0.95.2"
-../build/binbrew install goreleaser 0.95.2
+../build/binbrew install goreleaser@0.95.2
 
 echo "Installing gotestsum 0.3.2"
-../build/binbrew install gotestsum 0.3.2
+../build/binbrew install gotestsum@0.3.2
